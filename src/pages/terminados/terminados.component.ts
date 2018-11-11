11c3720c97cb9580d46807fb155d4141e4ed5adb
yojotaman <yojotaman@gmail.com>
2018-11-11 14:56:49 -0500

    Se agrega estilo a las dos vistas y se conecta el servicio al componente de terminados

--- src/pages/terminados/terminados.component.ts
+++ src/pages/terminados/terminados.component.ts
@@ -1,9 +1,18 @@
 import { Component } from "@angular/core";
+import { DeseosService } from '../../services/deseos.service';
+import { Lista } from "../../models";
 
 @Component({
     selector: 'page-terminados',
     templateUrl: 'terminados.component.html'
 })
 export class TerminadosPage {
+    constructor( public deseosService: DeseosService) {
+
+    }
+
+    listaSeleccionada( lista: Lista ){
+        console.log(lista)
+    }
 
 }
