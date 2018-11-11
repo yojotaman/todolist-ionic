37ec12c77b0bbceac8cac021888528ec27e6e6f4
yojotaman <yojotaman@gmail.com>
2018-11-11 12:33:36 -0500

    Se crean la estructura base de la app, se crean las  páginas pendientes y terminados y se actualizan los tabs

---
+++ src/pages/pendientes/pendientes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+    selector: 'page-pendientes',
+    templateUrl: 'pendientes.component.html'
+})
+
+export class PendientesPage {
+
+}
