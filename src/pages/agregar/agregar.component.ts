cb2efb07541281cbf607f0523263a0022022929e
yojotaman <yojotaman@gmail.com>
2018-11-13 11:04:57 -0500

    Se agrega funcionalidad de agregar items a la lista y se añade checkbox para marca de completado

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { DeseosService } from '../../services/deseos.service';
 import { Lista } from "../../models";
 import { NavParams } from "ionic-angular";
+import { ListaItem } from '../../models';
 
 
 
@@ -12,11 +13,29 @@ import { NavParams } from "ionic-angular";
 
 export class AgregarPage {
     lista: Lista
+    nombreItem: string = '';
+    
+    
     constructor(public deseosService: DeseosService,
                 private navParams: NavParams ){
-
+        
+        // console.log(this.navParams);
         const titulo = this.navParams.get('titulo');
         this.lista = new Lista(titulo);
+    }
+
+    agregarItem(){
+        if(this.nombreItem.length === 0) {
+            return;
+        }
+        const nuevoItem = new ListaItem(this.nombreItem); 
+        this.lista.items.push(nuevoItem)
+        console.log(this.lista.items);
+        this.nombreItem= '';
+    }
+
+    actualizarTarea(item: ListaItem){
+        item.completado = !item.completado
     }
 
     
